dc7ece5678185a3a53c92ef1f716587fe6f206dd
Aliaksandra Halinouskaya <aliaksandra_halinouskaya@epam.com>
2019-05-23 12:44:40 +0300

    Created DriverSingleton

--- src/test/java/com/epam/tat21/crypto/driver/DriverSingleton.java
+++ src/test/java/com/epam/tat21/crypto/driver/DriverSingleton.java
@@ -9,7 +9,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverSingleton {
 	private static WebDriver driver;
-	public static final String BROWSER = "browser";
 
 	private DriverSingleton() {
 	}
@@ -22,14 +21,17 @@ public class DriverSingleton {
 				WebDriverManager.firefoxdriver().setup();
 				driver = new FirefoxDriver();
 			}
+				break;
 			case "chrome": {
 				WebDriverManager.chromedriver().setup();
 				driver = new ChromeDriver();
 			}
+				break;
 			case "edge": {
 				WebDriverManager.edgedriver().setup();
 				driver = new EdgeDriver();
 			}
+				break;
 			default: {
 				WebDriverManager.chromedriver().setup();
 				driver = new ChromeDriver();
