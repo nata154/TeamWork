fa76e5e0aa954fbb8ec83a9a00dbe59322a1bfb7
Aliaksandra Halinouskaya <aliaksandra_halinouskaya@epam.com>
2019-05-24 12:59:16 +0300

    DriverSingleton has been renamed to DriverProvider

--- src/test/java/com/epam/tat21/crypto/driver/DriverProvider.java
+++ src/test/java/com/epam/tat21/crypto/driver/DriverProvider.java
@@ -7,10 +7,10 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class DriverSingleton {
+public class DriverProvider {
 	private static WebDriver driver;
 
-	private DriverSingleton() {
+	private DriverProvider() {
 	}
 
 	public static WebDriver getDriver() {
