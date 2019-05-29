9ef8b5c2be1497ab10c3defd2a8726811446c3c3
Natallia Karneyeva <Natallia_Karneyeva1@epam.com>
2019-05-29 14:03:41 +0300

    Refactor TestDataReader

--- src/test/java/com/epam/tat21/crypto/service/TestDataReader.java
+++ src/test/java/com/epam/tat21/crypto/service/TestDataReader.java
@@ -3,13 +3,19 @@ package com.epam.tat21.crypto.service;
 import java.util.ResourceBundle;
 
 public class TestDataReader {
+    public static final String TESTDATA_USER_NAME = "testdata.user.name";
+    public static final String TESTDATA_USER_PASSWORD = "testdata.user.password";
     private static ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getProperty("environment"));
 
-    public static String getUserName(String key) {
+    public static String getProperty(String key){
         return resourceBundle.getString(key);
     }
 
-    public static String getUserPassword(String key) {
-        return resourceBundle.getString(key);
+    public static String getUserName() {
+        return getProperty(TESTDATA_USER_NAME);
+    }
+
+    public static String getUserPassword() {
+        return getProperty(TESTDATA_USER_PASSWORD);
     }
 }
