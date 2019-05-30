eacc440b7b660fabaf92e06c6227f6edbd8dcbfe
Aliaksandr Posakhau <aliaksandr_posakhau@epam.com>
2019-05-30 16:34:59 +0300

    adding task files to Posakhau branch

--- src/test/java/com/epam/tat21/crypto/tests/VerifyApplicationTitleTest.java
+++ src/test/java/com/epam/tat21/crypto/tests/VerifyApplicationTitleTest.java
@@ -7,7 +7,7 @@ public class VerifyApplicationTitleTest extends BaseTest{
     private static String APP_TITLE = "CryptoCompare.com - Live cryptocurrency prices, trades, volumes, forums, wallets, mining equipment, and reviews | CryptoCompare.com";
 
     @Test
-    public void VerifyApplicationTitleTest() {
+    public void verifyApplicationTitleTest() {
         Assert.assertEquals(driver.getTitle(),APP_TITLE);
     }
 }
---
+++ src/test/java/com/epam/tat21/crypto/utils/SimpleCoin.java
@@ -0,0 +1,36 @@
+package com.epam.tat21.crypto.utils;
+
+public class SimpleCoin {
+
+    private String coinName="Default Coin";
+    private double coinTotalValue = 0;
+
+    public SimpleCoin(String name, Double value) {
+        setCoinName(name);
+        setCoinTotalValue(value);
+    }
+
+    public String getCoinName() {
+        return coinName;
+    }
+
+    public void setCoinName(String coinName) {
+        this.coinName = coinName;
+    }
+
+    public double getCoinTotalValue() {
+        return coinTotalValue;
+    }
+
+    public void setCoinTotalValue(double coinTotalValue) {
+        this.coinTotalValue = coinTotalValue;
+    }
+
+    public void coinReport() {
+        System.out.println("COIN NAME - "+getCoinName());
+        System.out.println("COIN TOTAL VALUE - "+getCoinTotalValue());
+        System.out.println("=========================================");
+    }
+
+
+}
