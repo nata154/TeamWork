4ae55f147eb64798d824cacde6ce957005eacb22
Natallia Karneyeva <Natallia_Karneyeva1@epam.com>
2019-05-30 15:59:25 +0300

    TestDataReader updated according to ProfilePage

--- src/test/java/com/epam/tat21/crypto/service/TestDataReader.java
+++ src/test/java/com/epam/tat21/crypto/service/TestDataReader.java
@@ -6,6 +6,7 @@ public class TestDataReader {
     private static final String TESTDATA_USER_NAME = "testdata.user.name";
     private static final String TESTDATA_USER_PASSWORD = "testdata.user.password";
     private static final String APPLICATION_URL = "application.url";
+    private static final String PROFILE_NAME = "profile.name";
     private static ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getProperty("environment"));
 
     private TestDataReader() {
@@ -25,5 +26,9 @@ public class TestDataReader {
 
     public static String getApplicationUrl() {
         return getProperty(APPLICATION_URL);
+    }
+
+    public static String getProfileName() {
+        return getProperty(PROFILE_NAME);
     }
 }
