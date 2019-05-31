660faaf90b2c020ed310fe05d1ed16dda768c964
Krystsina Patapenka <krystsina_patapenka@epam.com>
2019-05-31 13:29:18 +0300

    Refactored FilterOfNewsTest

--- src/test/java/com/epam/tat21/crypto/tests/FilterOfNewsTest.java
+++ src/test/java/com/epam/tat21/crypto/tests/FilterOfNewsTest.java
@@ -1,37 +1,40 @@
 package com.epam.tat21.crypto.tests;
 
+import com.epam.tat21.crypto.bo.Coin;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class FilterOfNewsTest extends CommonConditions {
 
+    private static final int COUNT_OF_NEWS = 50;
+
     @Test
     public void checkFilterNewsByBitcoinTest() {
-        Assert.assertTrue(steps.checkFilterNewsByBitcoin());
+        Assert.assertEquals(steps.checkFilterNewsByCoin(Coin.BTC), COUNT_OF_NEWS);
     }
 
     @Test
     public void checkFilterNewsByEthereumTest() {
-        Assert.assertTrue(steps.checkFilterNewsByEthereum());
+        Assert.assertEquals(steps.checkFilterNewsByCoin(Coin.ETH), COUNT_OF_NEWS);
     }
 
     @Test
     public void checkFilterNewsByLitecoinTest() {
-        Assert.assertTrue(steps.checkFilterNewsByLitecoin());
+        Assert.assertEquals(steps.checkFilterNewsByCoin(Coin.LTC), COUNT_OF_NEWS);
     }
 
     @Test
     public void checkFilterNewsByMoneroTest() {
-        Assert.assertTrue(steps.checkFilterNewsByMonero());
+        Assert.assertEquals(steps.checkFilterNewsByCoin(Coin.XMR), COUNT_OF_NEWS);
     }
 
     @Test
     public void checkFilterNewsByZCashTest() {
-        Assert.assertTrue(steps.checkFilterNewsByZCash());
+        Assert.assertEquals(steps.checkFilterNewsByCoin(Coin.ZEC), COUNT_OF_NEWS);
     }
 
     @Test
     public void checkFilterNewsByXRPTest() {
-        Assert.assertTrue(steps.checkFilterNewsByXRP());
+        Assert.assertEquals(steps.checkFilterNewsByCoin(Coin.XRP), COUNT_OF_NEWS);
     }
 }
