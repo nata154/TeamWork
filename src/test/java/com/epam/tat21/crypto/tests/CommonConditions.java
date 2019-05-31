43d22790dbdc9bcab73ee72263d3d53539ed7919
Krystsina Patapenka <krystsina_patapenka@epam.com>
2019-05-31 11:18:48 +0300

    Refactored CommonConditions class

--- src/test/java/com/epam/tat21/crypto/tests/CommonConditions.java
+++ src/test/java/com/epam/tat21/crypto/tests/CommonConditions.java
@@ -7,12 +7,11 @@ import org.testng.annotations.BeforeClass;
 public class CommonConditions {
 
     protected Steps steps;
-    private static final String TEAMWORK_APP_URL = "https://www.cryptocompare.com";
 
     @BeforeClass
     public void setUp() {
         steps = new Steps();
-        steps.openBrowser(TEAMWORK_APP_URL);
+        steps.openBrowser();
     }
 
     @AfterClass
