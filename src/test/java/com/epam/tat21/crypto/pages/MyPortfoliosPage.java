dedae38efe3d6ea0bad71e89685098cb1c229599
Aliaksandr Posakhau <aliaksandr_posakhau@epam.com>
2019-05-31 13:34:57 +0300

    confirmed

--- src/test/java/com/epam/tat21/crypto/pages/MyPortfoliosPage.java
+++ src/test/java/com/epam/tat21/crypto/pages/MyPortfoliosPage.java
@@ -2,7 +2,6 @@ package com.epam.tat21.crypto.pages;
 
 import com.epam.tat21.crypto.utils.PortfolioKeeper;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -20,28 +19,26 @@ public class MyPortfoliosPage {
     private static String userEmail = "testinglab2019cw10@mail.ru";
     private static String userPassword = "xenocrypt";
     private static int WAIT_FOR_ELEMENT_SECONDS = 15;
-
+    public PortfolioKeeper portfolioKeeper = new PortfolioKeeper();
     private String loginButtonXpath = "(//a[contains(.,'Login')])[3]";
     private String emailFieldXpath = "(//input[@type='email'])[1]";
     private String passwordFieldXpath = "//input[contains(@type,'password')]";
     private String submitButtonXpath = "//button[@type='submit'][contains(.,'Login')]";
     private String portfolioLinkXpath = "//a[@ng-mouseover='menuPortfolio=true']";
-
     private WebDriver driver;
-
     private WebElement loginButton;
     private WebElement portfolioLink;
-
     private WebElement emailField;
     private WebElement passwordField;
     private WebElement submitButton;
-
     private String portfolioName = "PORTFOLIO-NAVY";
 
-    public PortfolioKeeper portfolioKeeper = new PortfolioKeeper();
+//    public MyPortfoliosPage(WebDriver driver) {
+//        //super(driver);
+//    }
 
-    public MyPortfoliosPage(WebDriver driver) {
-        //super(driver);
+    protected static WebElement waitForElementBeClickableLocatedByXpath(WebDriver driver, String xPath) {
+        return new WebDriverWait(driver, WAIT_FOR_ELEMENT_SECONDS).until(ExpectedConditions.elementToBeClickable(By.xpath(xPath)));
     }
 
     @BeforeTest
@@ -55,17 +52,17 @@ public class MyPortfoliosPage {
     public void myPortfoliosPageTest() {
         loginAndProceedToMyPortfoliosPage();
         portfolioKeeper.initiate(driver);
-        System.out.println("PORTFOLIO TOTAL VALUE - "+portfolioKeeper.getTotalValue(portfolioName));
-        Assert.assertEquals(0,0);
+        System.out.println("PORTFOLIO TOTAL VALUE - " + portfolioKeeper.getTotalValue(portfolioName));
+        Assert.assertEquals(0, 0);
     }
 
     private void loginAndProceedToMyPortfoliosPage() {
-        loginButton = waitForElementBeClickableLocatedByXpath(driver,loginButtonXpath);
+        loginButton = waitForElementBeClickableLocatedByXpath(driver, loginButtonXpath);
         loginButton.click();
 
-        emailField = waitForElementBeClickableLocatedByXpath(driver,emailFieldXpath);
-        passwordField = waitForElementBeClickableLocatedByXpath(driver,passwordFieldXpath);
-        submitButton = waitForElementBeClickableLocatedByXpath(driver,submitButtonXpath);
+        emailField = waitForElementBeClickableLocatedByXpath(driver, emailFieldXpath);
+        passwordField = waitForElementBeClickableLocatedByXpath(driver, passwordFieldXpath);
+        submitButton = waitForElementBeClickableLocatedByXpath(driver, submitButtonXpath);
 
         emailField.sendKeys(userEmail);
         passwordField.sendKeys(userPassword);
@@ -84,12 +81,8 @@ public class MyPortfoliosPage {
         return portfolioKeeper.getTotalValue(name);
     }
 
-    protected static WebElement waitForElementBeClickableLocatedByXpath(WebDriver driver, String xPath) {
-        return new WebDriverWait(driver,WAIT_FOR_ELEMENT_SECONDS).until(ExpectedConditions.elementToBeClickable(By.xpath(xPath)));
-    }
-
     @AfterTest
     public void getSystemDown() {
-      driver.quit();
+        driver.quit();
     }
 }
