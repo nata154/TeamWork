db42b455e713d1c33c297d270ef61e2439e5a00c
Dzmitry Panimash <dv.panimash@gmail.com>
2019-06-03 14:50:00 +0300

    test methods changed in appliance with comments

--- src/test/java/com/epam/tat21/crypto/tests/CountryFilterOnExchangePageTests.java
+++ src/test/java/com/epam/tat21/crypto/tests/CountryFilterOnExchangePageTests.java
@@ -1,17 +1,29 @@
 package com.epam.tat21.crypto.tests;
 
+import com.epam.tat21.crypto.bo.Countries;
+import com.epam.tat21.crypto.data.CountriesDataProvider;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class CountryFilterOnExchangePageTests extends CommonConditions{
+public class CountryFilterOnExchangePageTests extends CommonConditions {
 
-    @Test
-    public void isExchangesNumberOnFilteredPageEqualNumberFromBadgeTest() {
-        Assert.assertTrue(steps.isExchangesNumberOnFilteredPageEqualNumberFromBadge());
+    @Test(dataProvider = "countriesForTests", dataProviderClass = CountriesDataProvider.class)
+    public void isExchangesNumberOnFilteredPageEqualNumberFromBadgeTest(Countries country) {
+        steps.openExchangePage();
+        steps.filterByCountry(country);
+        int numberOfResultWithCountry = steps.getFromFilteredPageNumberOfResultsWith(country);
+        int numberFromCountryBadge = steps.getNumberOfExchangesFromCountryBadge();
+        Assert.assertEquals(numberOfResultWithCountry, numberFromCountryBadge);
     }
 
-    @Test
-    public void isFilteredPageContainsOnlyExchangesFromNeededCountryTest() {
-        Assert.assertTrue(steps.isFilteredPageContainsOnlyExchangesFromNeededCountry());
+    @Test(dataProvider = "countriesForTests", dataProviderClass = CountriesDataProvider.class)
+    public void isFilteredPageContainsOnlyExchangesFromNeededCountryTest(Countries country) {
+        steps.openExchangePage();
+        steps.filterByCountry(country);
+        int numberOfResultWithCountry = steps.getFromFilteredPageNumberOfResultsWith(country);
+        int numberOfAllCountryLabels = steps.getAllCountryLabelsFromFilteredPage();
+        Assert.assertEquals(numberOfResultWithCountry, numberOfAllCountryLabels);
     }
+
+
 }
