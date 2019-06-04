bf872a74273548ed2c94f6894dc05349db6afec9
Natallia Karneyeva <Natallia_Karneyeva1@epam.com>
2019-06-04 12:28:55 +0300

    Created test CheckUpdatingUserAccountTest

--- src/test/java/com/epam/tat21/crypto/tests/CheckUpdatingUserAccountTest.java
+++ src/test/java/com/epam/tat21/crypto/tests/CheckUpdatingUserAccountTest.java
@@ -1,38 +1,25 @@
 package com.epam.tat21.crypto.tests;
 
-import com.epam.tat21.crypto.bo.User;
-import com.epam.tat21.crypto.pages.HeaderPage;
-import com.epam.tat21.crypto.pages.MainCryptoComparePage;
-import com.epam.tat21.crypto.pages.UserAccountPage;
-import com.epam.tat21.crypto.service.UserCreator;
+import com.epam.tat21.crypto.utils.RandomString;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class CheckUpdatingUserAccountTest extends BaseTest {
+public class CheckUpdatingUserAccountTest extends CommonConditions {
+
+    private static final int COUNT_OF_GENERATED_POSTFIX_LETTERS = 3;
 
     @Test
     public void checkUpdatingUserAccountTest() {
-        String firstNameToChange = "natanata";
-        String surnameToChange = "natanata";
-        String popupAfterSavingChangesInUserGeneralTabWhenSuccessfully = "Successfully updated your details";
+        String firstNameToChange = RandomString.generateRandomUserFirstName(COUNT_OF_GENERATED_POSTFIX_LETTERS);
+        String surnameToChange = RandomString.generateRandomUserSurname(COUNT_OF_GENERATED_POSTFIX_LETTERS);
+        String expectedInfoInPopupAfterSavingChangesInUserAccountWhenSuccessfully = "Successfully updated your details";
 
-        User user = UserCreator.withCredentialsFromProperty();
-        new MainCryptoComparePage(driver)
-                .login(user);
+        steps.loginUser();
+        steps.goToUserAccountProfile();
+        steps.changeAndSaveFirstNameSurnameInUserAccount(firstNameToChange, surnameToChange);
+        String actualInfoInPopupAfterSavingChangesInUserGeneralTab = steps.getInfoFromPopupWindowAfterSavingChangesInUserAccount();
 
-        HeaderPage headerPage = new HeaderPage(driver);
-
-      //  UserDropdownMenuInHeader userDropdownMenuInHeader = new UserDropdownMenuInHeader(driver);
-
-        headerPage.getDropdownUserMenuInHeader().clickAccountLine();
-
-        UserAccountPage userAccountPage = new UserAccountPage(driver);
-        userAccountPage.clickTabGeneralInUserAccount()
-                .enterNewFirstNameAndSurnameInTabGeneralInUserAccount(firstNameToChange, surnameToChange)
-                .clickButtonSaveChangesInGeneralTabInUserAccount();
-
-        Assert.assertEquals(userAccountPage.getInfoFromPopupWindowAfterSavingChangesInUserGeneralTab(), popupAfterSavingChangesInUserGeneralTabWhenSuccessfully,
+        Assert.assertEquals(actualInfoInPopupAfterSavingChangesInUserGeneralTab, expectedInfoInPopupAfterSavingChangesInUserAccountWhenSuccessfully,
                 "Wrong text in pop-up after saving Changes In User General Tab");
-
     }
 }
