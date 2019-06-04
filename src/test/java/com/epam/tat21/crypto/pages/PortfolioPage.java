8233a4cc3e10a370d1f1b439df56721629d256f3
Krystsina Patapenka <krystsina_patapenka@epam.com>
2019-06-04 11:53:30 +0300

    Refactored PortfolioPage

--- src/test/java/com/epam/tat21/crypto/pages/PortfolioPage.java
+++ src/test/java/com/epam/tat21/crypto/pages/PortfolioPage.java
@@ -2,10 +2,18 @@ package com.epam.tat21.crypto.pages;
 
 import com.epam.tat21.crypto.service.TestDataReader;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-public class PortfolioPage extends BasePage {
+public class PortfolioPage extends HeaderPage {
 
     private final String BASE_URL = TestDataReader.getApplicationUrl() + "portfolio/";
+
+    @FindBy(xpath = "//span[@class='ng-scope' and contains(text(), 'Coin')]")
+    private WebElement addCoinToPortfolioButton;
+
+    @FindBy(xpath = "//div[@class='list-table list-portfolio padding-5']")
+    private WebElement tableOfCoinsInPortfolio;
 
     public PortfolioPage(WebDriver driver) {
         super(driver);
@@ -15,5 +23,11 @@ public class PortfolioPage extends BasePage {
     public PortfolioPage openPage() {
         driver.get(BASE_URL);
         return this;
+    }
+
+    public AddCoinForm getAddCoinForm() {
+        waitForElementClicable(addCoinToPortfolioButton);
+        addCoinToPortfolioButton.click();
+        return new AddCoinForm(driver);
     }
 }
