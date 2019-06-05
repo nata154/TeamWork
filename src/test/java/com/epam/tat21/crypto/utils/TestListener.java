8ea37fd4eb16f7bf1926779c1152eef064ee7d8e
Natallia Karneyeva <Natallia_Karneyeva1@epam.com>
2019-06-05 15:32:07 +0300

    Created TestListener

---
+++ src/test/java/com/epam/tat21/crypto/utils/TestListener.java
@@ -0,0 +1,65 @@
+package com.epam.tat21.crypto.utils;
+
+import com.epam.tat21.crypto.driver.DriverProvider;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+//import com.epam.tat21.crypto.utils.MyLogger;
+
+public class TestListener implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+    }
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        saveScreenshot();
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+    }
+
+    private void saveScreenshot() {
+        File screenCapture = ((TakesScreenshot) DriverProvider.getDriver()).getScreenshotAs(OutputType.FILE);
+        try {
+            FileUtils.copyFile(screenCapture, new File(
+                    ".//target/screenshots/"
+                            + getCurrentTimeAsString() +
+                            ".png"));
+        } catch (IOException e) {
+//            MyLogger.error("Failed to save screenshot: " + e.getLocalizedMessage());
+        }
+    }
+
+    private String getCurrentTimeAsString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd_HH-mm-ss");
+        return ZonedDateTime.now().format(formatter);
+    }
+}
