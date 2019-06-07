b2c8200f5950ce02316247ba45774b7be7203bfb
Krystsina Patapenka <krystsina_patapenka@epam.com>
2019-06-07 10:59:08 +0300

    Created CoinsDataProvider class

---
+++ src/test/java/com/epam/tat21/crypto/data/CoinsDataProvider.java
@@ -0,0 +1,21 @@
+package com.epam.tat21.crypto.data;
+
+import com.epam.tat21.crypto.bo.Coin;
+import org.testng.annotations.DataProvider;
+
+public class CoinsDataProvider {
+
+    @DataProvider(name = "coinsForTests")
+    public static Object[][] coinNameForFilter() {
+
+        Object[][] toReturn = new Object[Coin.values().length][];
+        int i = 0;
+        for (Coin coin : Coin.values()) {
+            toReturn[i] = new Object[1];
+            toReturn[i][0] = coin;
+            i++;
+        }
+        return toReturn;
+    }
+
+}
