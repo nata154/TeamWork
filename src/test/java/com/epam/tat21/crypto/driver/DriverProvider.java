beeead4148a244f195378b97e0921bbeca7dc512
Krystsina Patapenka <krystsina_patapenka@epam.com>
2019-06-07 14:15:39 +0300

    Created getRemoteDriver method

--- src/test/java/com/epam/tat21/crypto/driver/DriverProvider.java
+++ src/test/java/com/epam/tat21/crypto/driver/DriverProvider.java
@@ -1,15 +1,24 @@
 
 package com.epam.tat21.crypto.driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static com.epam.tat21.crypto.service.TestDataReader.getNodeURL;
 
 public class DriverProvider {
     private static WebDriver driver;
+    private static String nodeURL = getNodeURL();
+    private static DesiredCapabilities capabilities;
 
     private DriverProvider() {
     }
@@ -39,6 +48,45 @@ public class DriverProvider {
                 }
             }
             driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    public static WebDriver getRemoteDriver() {
+        if (driver == null) {
+            switch (System.getProperty("browser")) {
+                case "firefox": {
+                    WebDriverManager.firefoxdriver().setup();
+                    capabilities = DesiredCapabilities.firefox();
+                    capabilities.setBrowserName("firefox");
+                }
+                break;
+                case "chrome": {
+                    WebDriverManager.chromedriver().setup();
+                    capabilities = DesiredCapabilities.chrome();
+                    capabilities.setBrowserName("chrome");
+
+                }
+                break;
+                case "edge": {
+                    WebDriverManager.edgedriver().setup();
+                    capabilities = DesiredCapabilities.edge();
+                    capabilities.setBrowserName("edge");
+                }
+                break;
+                default: {
+                    WebDriverManager.chromedriver().setup();
+                    capabilities = DesiredCapabilities.chrome();
+                    capabilities.setBrowserName("chrome");
+                }
+            }
+            try {
+                capabilities.setPlatform(Platform.WINDOWS);
+                driver = new RemoteWebDriver(new URL(nodeURL), capabilities);
+                driver.manage().window().maximize();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
         }
         return driver;
     }
