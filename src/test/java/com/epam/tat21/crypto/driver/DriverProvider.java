82db437b6f783c6e81c89fed881290f32e0202ff
Krystsina Patapenka <krystsina_patapenka@epam.com>
2019-06-11 13:24:00 +0300

    Refactored DriverProvider class

--- src/test/java/com/epam/tat21/crypto/driver/DriverProvider.java
+++ src/test/java/com/epam/tat21/crypto/driver/DriverProvider.java
@@ -1,92 +1,30 @@
 
 package com.epam.tat21.crypto.driver;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import static com.epam.tat21.crypto.service.TestDataReader.getNodeURL;
 
 public class DriverProvider {
     private static WebDriver driver;
-    private static String nodeURL = getNodeURL();
-    private static DesiredCapabilities capabilities;
 
     private DriverProvider() {
     }
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            switch (System.getProperty("browser")) {
+            switch (System.getProperty("driver")) {
+                case "local":
+                    driver = new LocalDriver().getDriver();
+                    break;
 
-                case "firefox": {
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                }
-                break;
-                case "chrome": {
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                }
-                break;
-                case "edge": {
-                    WebDriverManager.edgedriver().setup();
-                    driver = new EdgeDriver();
-                }
-                break;
-                default: {
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                }
+                case "remote":
+                    driver = new RemoteDriver().getDriver();
+                    break;
+
+                case "sauce":
+                    driver = new RemoteDriverSauceLabs().getDriver();
+                    break;
             }
             driver.manage().window().maximize();
-        }
-        return driver;
-    }
-
-    public static WebDriver getRemoteDriver() {
-        if (driver == null) {
-            switch (System.getProperty("browser")) {
-                case "firefox": {
-                    WebDriverManager.firefoxdriver().setup();
-                    capabilities = DesiredCapabilities.firefox();
-                    capabilities.setBrowserName("firefox");
-                }
-                break;
-                case "chrome": {
-                    WebDriverManager.chromedriver().setup();
-                    capabilities = DesiredCapabilities.chrome();
-                    capabilities.setBrowserName("chrome");
-
-                }
-                break;
-                case "edge": {
-                    WebDriverManager.edgedriver().setup();
-                    capabilities = DesiredCapabilities.edge();
-                    capabilities.setBrowserName("edge");
-                }
-                break;
-                default: {
-                    WebDriverManager.chromedriver().setup();
-                    capabilities = DesiredCapabilities.chrome();
-                    capabilities.setBrowserName("chrome");
-                }
-            }
-            try {
-                capabilities.setPlatform(Platform.WINDOWS);
-                driver = new RemoteWebDriver(new URL(nodeURL), capabilities);
-                driver.manage().window().maximize();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
         }
         return driver;
     }
