f78267430e5d9aa81972ba15c9e5eb97bacdc387
Krystsina Patapenka <krystsina_patapenka@epam.com>
2019-06-11 13:56:20 +0300

    Added private constructor in CapabilitiesProvider

--- src/test/java/com/epam/tat21/crypto/driver/CapabilitiesProvider.java
+++ src/test/java/com/epam/tat21/crypto/driver/CapabilitiesProvider.java
@@ -7,6 +7,9 @@ public class CapabilitiesProvider {
 
     private static DesiredCapabilities capabilities;
 
+    private CapabilitiesProvider() {
+    }
+
     public static DesiredCapabilities getCapabilities(String browser) {
         switch (browser) {
             case "firefox":
