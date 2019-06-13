8d1fc53f44d60a34253ac73b07a91739449754fc
Krystsina Patapenka <krystsina_patapenka@epam.com>
2019-06-13 13:42:32 +0300

    Refactored getFactory method

--- src/test/java/com/epam/tat21/crypto/steps/Steps.java
+++ src/test/java/com/epam/tat21/crypto/steps/Steps.java
@@ -27,8 +27,6 @@ public class Steps {
                     return new RemoteDriver();
                 case "sauce":
                     return new RemoteDriverSauceLabs();
-                default:
-                    return new LocalDriver();
             }
         }
         return new LocalDriver();
