d47775a0019b8e46bded1447d7bd45f73b547b60
Natallia Karneyeva <Natallia_Karneyeva1@epam.com>
2019-06-14 12:09:47 +0300

    add JIRATestKey to test

--- src/test/java/com/epam/tat21/crypto/tests/CountryFilterOnExchangePageTests.java
+++ src/test/java/com/epam/tat21/crypto/tests/CountryFilterOnExchangePageTests.java
@@ -2,11 +2,13 @@ package com.epam.tat21.crypto.tests;
 
 import com.epam.tat21.crypto.bo.Countries;
 import com.epam.tat21.crypto.data.CountriesDataProvider;
+import com.epam.tat21.crypto.utils.jira.JIRATestKey;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CountryFilterOnExchangePageTests extends CommonConditions {
 
+    @JIRATestKey(key = "EPMFARMATS-9116")
     @Test(dataProvider = "countriesForTests", dataProviderClass = CountriesDataProvider.class)
     public void isExchangesNumberOnFilteredPageEqualNumberFromBadgeTest(Countries country) {
         steps.openExchangePage();
@@ -16,6 +18,7 @@ public class CountryFilterOnExchangePageTests extends CommonConditions {
         Assert.assertEquals(numberOfResultWithCountry, numberFromCountryBadge);
     }
 
+    @JIRATestKey(key = "EPMFARMATS-9115")
     @Test(dataProvider = "countriesForTests", dataProviderClass = CountriesDataProvider.class)
     public void isFilteredPageContainsOnlyExchangesFromNeededCountryTest(Countries country) {
         steps.openExchangePage();
