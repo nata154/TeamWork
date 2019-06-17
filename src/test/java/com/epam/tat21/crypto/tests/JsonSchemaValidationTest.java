dcf69d0c77f4eee25906a3d3bb0d1d56d1127b6a
Aliaksandr Posakhau <aliaksandr_posakhau@epam.com>
2019-06-17 11:08:17 +0300

    ting the last changes

---
+++ src/test/java/com/epam/tat21/crypto/tests/JsonSchemaValidationTest.java
@@ -0,0 +1,16 @@
+package com.epam.tat21.crypto.tests;
+
+import org.testng.annotations.Test;
+
+public class JsonSchemaValidationTest extends CommonConditions {
+
+    private static final int COUNT_OF_GENERATED_POSTFIX_LETTERS = 3;
+
+    @Test(description = "EPMFARMATS-9277")
+    public void jsonSchemaValidationTest() {
+
+
+
+
+    }
+}
