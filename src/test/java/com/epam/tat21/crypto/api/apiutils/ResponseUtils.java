cb8b2bb603106f3c6110655971f9a45e583a0930
Dzmitry Panimash <dv.panimash@gmail.com>
2019-06-19 10:34:34 +0300

    renamed class ObjectCreatorFromResponse and added javadoc

--- src/test/java/com/epam/tat21/crypto/api/apiutils/ResponseUtils.java
+++ src/test/java/com/epam/tat21/crypto/api/apiutils/ResponseUtils.java
@@ -5,11 +5,17 @@ import io.restassured.response.Response;
 
 import java.io.IOException;
 
-public class ObjectCreatorFromResponse {
+public class ResponseUtils {
 
-    private ObjectCreatorFromResponse() {
+    private ResponseUtils() {
     }
 
+    /**
+     * A method below serves for filling model classes from response.
+     * It receives Response object and a model class, which filling is needed,
+     * then with jackson library reads the response, fills the model class
+     * and after all returns it.
+     */
     public static <T> T getObjectFromResponse(Response response, Class<T> tClass) throws IOException {
         return new ObjectMapper().readValue(response.getBody().asString(), tClass);
     }
