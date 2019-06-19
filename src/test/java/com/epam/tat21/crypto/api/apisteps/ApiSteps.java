904152b32b27683d0e266db196434a6dec8507ca
Dzmitry Panimash <dv.panimash@gmail.com>
2019-06-19 10:35:23 +0300

    refactored apisteps

--- src/test/java/com/epam/tat21/crypto/api/apisteps/ApiSteps.java
+++ src/test/java/com/epam/tat21/crypto/api/apisteps/ApiSteps.java
@@ -1,13 +1,15 @@
 package com.epam.tat21.crypto.api.apisteps;
 
-import com.epam.tat21.crypto.api.apiutils.ObjectCreatorFromResponse;
+import com.epam.tat21.crypto.api.apiutils.ResponseUtils;
 import com.epam.tat21.crypto.api.model.LatestNews;
+import com.epam.tat21.crypto.api.model.NewsItem;
 import com.epam.tat21.crypto.service.TestDataReader;
 import com.epam.tat21.crypto.utils.MyLogger;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class ApiSteps {
@@ -22,13 +24,14 @@ public class ApiSteps {
         MyLogger.info("Filling model classes LatestNews -> NewsItem -> NewsItemSourceInfo");
         Response response = getResponseWithLatestNews();
         //with Jackson library serialize a tree of model classes
-        return ObjectCreatorFromResponse.getObjectFromResponse(response, LatestNews.class);
+        return ResponseUtils.getObjectFromResponse(response, LatestNews.class);
     }
 
     public String[] getLatestNewsTitleItems(int numberOfItems) throws IOException {
-        MyLogger.info("Getting 50 latest news titles from the response");
+        MyLogger.info("Getting " + numberOfItems + " latest news titles from the response");
         LatestNews latestNews = getLatestNewsFromResponse();
+        List<NewsItem> sortedNews = latestNews.getSortedData();
         //return subarray of titles, cause the news page can contain 50 news, while JSON can contain 100
-        return IntStream.range(0, numberOfItems).mapToObj(i -> latestNews.getSortedData().get(i).getTitle()).toArray(String[]::new);
+        return IntStream.range(0, numberOfItems).mapToObj(i -> sortedNews.get(i).getTitle()).toArray(String[]::new);
     }
 }
