55395d780013deb1b0a60a557ce3dc72e7a06010
Dzmitry Panimash <dv.panimash@gmail.com>
2019-06-19 10:36:32 +0300

    mistakes are corrected

--- src/test/java/com/epam/tat21/crypto/api/apiutils/NewsItemComparator.java
+++ src/test/java/com/epam/tat21/crypto/api/apiutils/NewsItemComparator.java
@@ -10,15 +10,15 @@ public class NewsItemComparator implements Comparator<NewsItem> {
      * News on the page are printed by their published time.
      * As bigger the time is, as higher the news is. If the news have the same time,
      * their order depends on id: as less id is - as higher the news is. It allows
-     * to avoid mismatching between yhe news page and the api response.
+     * to avoid mismatching between the news page and the api response.
      */
 
     @Override
     public int compare(NewsItem n1, NewsItem n2) {
-        int time1 = n1.getPublished_on();
-        int time2 = n2.getPublished_on();
-        int id1 = n1.getPublished_on();
-        int id2 = n2.getPublished_on();
+        int time1 = n1.getPublishedOn();
+        int time2 = n2.getPublishedOn();
+        int id1 = Integer.parseInt(n1.getId());
+        int id2 = Integer.parseInt(n2.getId());
         if (time1 > time2) {
             return -1;
         } else if (time1 < time2) {
