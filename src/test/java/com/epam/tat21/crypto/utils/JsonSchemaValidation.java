4e42d9e0e553f608309379d4ca825fd842c7b527
Aliaksandr Posakhau <aliaksandr_posakhau@epam.com>
2019-06-21 18:15:51 +0300

    adding custom exceptions and refactor JSON Schema validation method

--- src/test/java/com/epam/tat21/crypto/utils/JsonSchemaValidation.java
+++ src/test/java/com/epam/tat21/crypto/utils/JsonSchemaValidation.java
@@ -1,4 +1,6 @@
 package com.epam.tat21.crypto.utils;
+
+import com.epam.tat21.crypto.exceptions.JsonSchemaValidationException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.github.fge.jackson.JsonLoader;
 import com.github.fge.jsonschema.core.exceptions.ProcessingException;
@@ -12,31 +14,39 @@ import java.net.URL;
 
 public class JsonSchemaValidation {
 
-    public static boolean validateJsonSchema(String jsonUrlPath, String jsonSchemaPath) throws IOException {
+    public static boolean validateJsonSchema(String jsonUrlPath, String jsonSchemaPath) {
         return validate(loadJsonObject(jsonUrlPath), loadJsonSchema(jsonSchemaPath));
     }
 
-    private static JsonNode loadJsonObject(String jsonUrlPath) throws IOException {
-        return JsonLoader.fromURL(new URL(jsonUrlPath));
+    private static JsonNode loadJsonObject(String jsonUrlPath) {
+        try {
+            return JsonLoader.fromURL(new URL(jsonUrlPath));
+        } catch (IOException e) {
+            throw new JsonSchemaValidationException("Cannot load JSON object from URL");
+        }
     }
 
-    private static JsonNode loadJsonSchema(String jsonSchemaPath) throws IOException {
-        return JsonLoader.fromPath((new File(jsonSchemaPath).getAbsolutePath()).replaceAll("//", File.separator));
+    private static JsonNode loadJsonSchema(String jsonSchemaPath) {
+        try {
+            return JsonLoader.fromPath((new File(jsonSchemaPath).getAbsolutePath()).replaceAll("//", File.separator));
+        } catch (IOException e) {
+            throw new JsonSchemaValidationException("Cannot load JSON Schema from current path");
+        }
     }
 
     private static boolean validate(JsonNode jsonObject, JsonNode jsonSchema) {
         final JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
-        JsonSchema schema = null;
+        JsonSchema schema;
         try {
             schema = factory.getJsonSchema(jsonSchema);
         } catch (ProcessingException e) {
-            e.printStackTrace();
+            throw new JsonSchemaValidationException("Entity of JSON Schema cannot be initialized from JsonSchemaFactory entity");
         }
-        ProcessingReport report = null;
+        ProcessingReport report;
         try {
             report = schema.validate(jsonObject);
         } catch (ProcessingException e) {
-            e.printStackTrace();
+            throw new JsonSchemaValidationException("Error objects validation");
         }
 
         if (report.isSuccess()) {
