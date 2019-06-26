ef1a6783ba5aece06d36f63f29db8076359e452e
Dzmitry Panimash <dv.panimash@gmail.com>
2019-06-26 13:37:53 +0300

    added test for feeds

--- src/test/java/com/epam/tat21/crypto/api/apitests/MatchingLatestNewsTests.java
+++ src/test/java/com/epam/tat21/crypto/api/apitests/MatchingLatestNewsTests.java
@@ -1,22 +1,25 @@
 package com.epam.tat21.crypto.api.apitests;
 
 import com.epam.tat21.crypto.api.apidata.FeedsNamesAndKeysProvider;
+import com.epam.tat21.crypto.bo.Coin;
+import com.epam.tat21.crypto.data.CoinsDataProvider;
 import com.epam.tat21.crypto.tests.CommonConditions;
 import com.epam.testng.JIRATestKey;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class MatchingLatestNewsTests extends CommonConditions {
 
-   /* @JIRATestKey(key = "EPMFARMATS-9289")
+    @JIRATestKey(key = "EPMFARMATS-9289")
     @Test
     public void matchingLatestNewsInResponseAndOnPageByTitleTest() throws IOException {
         steps.openNewsPage();
-        String[] newsTitlesFromResponse = apiSteps.getLatestNewsTitleItems(50);
-        String[] newsTitlesFromPage = steps.getLatestNewsTitleItemsFromPage(50);
-        Assert.assertTrue(Arrays.equals(newsTitlesFromResponse, newsTitlesFromPage));
+        String[] newsTitlesFromResponse = apiSteps.getLatestNewsTitleItems();
+        String[] newsTitlesFromPage = steps.getLatestNewsTitleItemsFromPage();
+        Assert.assertTrue(Arrays.equals(newsTitlesFromPage, newsTitlesFromResponse));
     }
 
     @JIRATestKey(key = "EPMFARMATS-9296")
@@ -24,10 +27,10 @@ public class MatchingLatestNewsTests extends CommonConditions {
     public void matchingNewsInResponseAndOnPageByCoinTest(Coin coin) throws IOException {
         steps.openNewsPage();
         steps.filterByCoin(coin);
-        String[] newsTitlesFromResponse = apiSteps.getCoinNewsTitleItems(50, coin);
-        String[] newsTitlesFromPage = steps.getLatestNewsTitleItemsFromPage(50);
-        Assert.assertTrue(Arrays.equals(newsTitlesFromResponse, newsTitlesFromPage));
-    }*/
+        String[] newsTitlesFromResponse = apiSteps.getCoinNewsTitleItems(coin);
+        String[] newsTitlesFromPage = steps.getLatestNewsTitleItemsFromPage();
+        Assert.assertTrue(Arrays.equals(newsTitlesFromPage, newsTitlesFromResponse));
+    }
 
     @JIRATestKey(key = "EPMFARMATS-9341")
     @Test(dataProvider = "namesAndKeysForFeedsTests", dataProviderClass = FeedsNamesAndKeysProvider.class)
@@ -38,19 +41,6 @@ public class MatchingLatestNewsTests extends CommonConditions {
         String[] newsTitlesFromResponse = apiSteps.getFeedsNewsTitleItems(feedQueryKey);
         String[] newsTitlesFromPage = steps.getLatestNewsTitleItemsFromPage();
         System.out.println(newsTitlesFromResponse.length + "/response/-" + newsTitlesFromPage.length + "/page/");
-        /*for (String name : newsTitlesFromResponse) {
-            System.out.print(name + "/r/");
-        }
-        System.out.println();
-        for (String name : newsTitlesFromPage) {
-            System.out.print(name + "/p/");
-        }*/
-
-        //Assert.assertTrue(Arrays.equals(newsTitlesFromResponse, newsTitlesFromPage));
-
-        for (int i = 0; i < newsTitlesFromResponse.length; i++) {
-            Assert.assertEquals(newsTitlesFromResponse[i], newsTitlesFromPage[i]);
-        }
+        Assert.assertTrue(Arrays.equals(newsTitlesFromPage, newsTitlesFromResponse));
     }
-
 }
