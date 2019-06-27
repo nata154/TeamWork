1a334deade89952f4a4c4a27118ab49b3e8a10f7
Dzmitry Panimash <dv.panimash@gmail.com>
2019-06-27 10:56:03 +0300

    refactored test: feedQueryKey-> feedId

--- src/test/java/com/epam/tat21/crypto/api/apitests/MatchingLatestNewsTests.java
+++ src/test/java/com/epam/tat21/crypto/api/apitests/MatchingLatestNewsTests.java
@@ -1,6 +1,6 @@
 package com.epam.tat21.crypto.api.apitests;
 
-import com.epam.tat21.crypto.api.apidata.FeedsNamesAndKeysProvider;
+import com.epam.tat21.crypto.api.apidata.FeedsNamesAndIdProvider;
 import com.epam.tat21.crypto.bo.Coin;
 import com.epam.tat21.crypto.data.CoinsDataProvider;
 import com.epam.tat21.crypto.tests.CommonConditions;
@@ -33,14 +33,13 @@ public class MatchingLatestNewsTests extends CommonConditions {
     }
 
     @JIRATestKey(key = "EPMFARMATS-9341")
-    @Test(dataProvider = "namesAndKeysForFeedsTests", dataProviderClass = FeedsNamesAndKeysProvider.class)
-    public void matchingNewsByFeedsTest(String feedQueryKey, String feedName) throws IOException {
+    @Test(dataProvider = "namesAndIdForFeedsTests", dataProviderClass = FeedsNamesAndIdProvider.class)
+    public void matchingNewsByFeedsTest(String feedId, String feedName) throws IOException {
         steps.openNewsPage()
                 .clickOnFeedsDropdown()
                 .clickOnFeedLink(feedName);
-        String[] newsTitlesFromResponse = apiSteps.getFeedsNewsTitleItems(feedQueryKey);
+        String[] newsTitlesFromResponse = apiSteps.getFeedsNewsTitleItems(feedId);
         String[] newsTitlesFromPage = steps.getLatestNewsTitleItemsFromPage();
-        System.out.println(newsTitlesFromResponse.length + "/response/-" + newsTitlesFromPage.length + "/page/");
         Assert.assertTrue(Arrays.equals(newsTitlesFromPage, newsTitlesFromResponse));
     }
 }
