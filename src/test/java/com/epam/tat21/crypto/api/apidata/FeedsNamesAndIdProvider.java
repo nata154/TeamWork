afbd4fd39229ba7e11823e693f0c0c1d49ccb66a
Dzmitry Panimash <dv.panimash@gmail.com>
2019-06-27 10:54:41 +0300

    refactored provider: key->id

---
+++ src/test/java/com/epam/tat21/crypto/api/apidata/FeedsNamesAndIdProvider.java
@@ -0,0 +1,32 @@
+package com.epam.tat21.crypto.api.apidata;
+
+import com.epam.tat21.crypto.api.apisteps.ApiSteps;
+import com.epam.tat21.crypto.utils.MyLogger;
+import org.testng.annotations.DataProvider;
+
+import java.io.IOException;
+
+public class FeedsNamesAndIdProvider {
+
+    private static ApiSteps apiSteps = new ApiSteps();
+
+    private FeedsNamesAndIdProvider() {
+    }
+
+    @DataProvider(name = "namesAndIdForFeedsTests")
+    public static Object[][] namesAndIdForFeeds() throws IOException {
+        MyLogger.info("Initializing of the data provider: namesAndIdForFeedsTests");
+        String[] feedId = apiSteps.getAllFeedsId();
+        String[] feedNames = apiSteps.getAllFeedsNames();
+        Object[][] toReturn = new Object[feedId.length][];
+        int i = 0;
+        for (String value : feedId) {
+            toReturn[i] = new Object[2];
+            toReturn[i][0] = feedId[i];
+            toReturn[i][1] = feedNames[i];
+            i++;
+        }
+        return toReturn;
+    }
+
+}
