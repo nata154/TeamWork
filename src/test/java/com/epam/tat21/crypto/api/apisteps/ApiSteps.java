341a17b78f464dc5b5cd6eb72d5911a80cc477ce
Natallia Karneyeva <Natallia_Karneyeva1@epam.com>
2019-07-02 12:13:07 +0300

    delete compareMultiPricesWithDelta from apiSteps

--- src/test/java/com/epam/tat21/crypto/api/apisteps/ApiSteps.java
+++ src/test/java/com/epam/tat21/crypto/api/apisteps/ApiSteps.java
@@ -10,10 +10,8 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import static com.epam.tat21.crypto.service.GlobalConstants.REGEX_FOR_SPACES;
 
@@ -143,24 +141,4 @@ public class ApiSteps {
         return getMultiPriceFromResponse(getResponseWithMultiPrice(coinAbbreviations, currencyAbbreviations));
     }
 
-    public boolean compareMultiPricesWithDelta(Map<String, Map<String, Double>> multiPricesFromPageAsArray, Map<String, Map<String, Double>> multiPriceResponseAsArray, List<Coin> coins, List<Currency> currencies, double deltaExpected) {
-        boolean resultCompareMaps = true;
-        for (int i = 0; i < coins.size(); i++) {
-            for (int j = 0; j < currencies.size(); j++) {
-                double coinCostInCurrencyFromPage = multiPricesFromPageAsArray.get(coins.get(i).getAbbreviationCoin()).get(currencies.get(j).getNameOfCurrency());
-                double coinCostInCurrencyFromResponse = multiPriceResponseAsArray.get(coins.get(i).getAbbreviationCoin()).get(currencies.get(j).getNameOfCurrency());
-                double deltaActual = ((Math.abs(coinCostInCurrencyFromPage - coinCostInCurrencyFromResponse)) / coinCostInCurrencyFromPage) * 100;
-
-                BigDecimal bigDecimal = new BigDecimal(deltaActual);
-                BigDecimal deltaActualRounded = bigDecimal.setScale(4, BigDecimal.ROUND_HALF_UP);
-
-                MyLogger.info("Difference between current and expected values for " + coins.get(i).getAbbreviationCoin() + " in currency " + currencies.get(j).getNameOfCurrency() + " is: " + deltaActualRounded + ".");
-                if (deltaActual > deltaExpected) {
-                    MyLogger.error("For " + coins.get(i).getAbbreviationCoin() + " in currency " + currencies.get(j).getNameOfCurrency() + " difference between current and expected values is: " + deltaActualRounded + ". But expected no more than " + deltaExpected + " %.");
-                    resultCompareMaps = false;
-                }
-            }
-        }
-        return resultCompareMaps;
-    }
 }
