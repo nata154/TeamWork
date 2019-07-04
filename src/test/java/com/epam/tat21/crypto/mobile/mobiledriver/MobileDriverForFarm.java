1c4c2ffaae01cd166ee52cd6c6cfbc63a3233f1f
Krystsina Patapenka <krystsina_patapenka@epam.com>
2019-07-04 11:11:34 +0300

    Created MobileDriverForFarm class

---
+++ src/test/java/com/epam/tat21/crypto/mobile/mobiledriver/MobileDriverForFarm.java
@@ -0,0 +1,46 @@
+package com.epam.tat21.crypto.mobile.mobiledriver;
+
+import com.epam.tat21.crypto.utils.MyLogger;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import static com.epam.tat21.crypto.service.TestDataReader.*;
+
+public class MobileDriverForFarm implements MobileDriverFactory {
+
+    private static AppiumDriver<MobileElement> appiumDriver = null;
+    private DesiredCapabilities capabilities;
+
+    public MobileDriverForFarm() {
+        capabilities = MobCapabilitiesProvider.getCapabilities();
+        capabilities.setCapability(MobileCapabilityType.UDID, getMobileDeviceUuid());
+        capabilities.setCapability("platformVersion", "8.0.0");
+    }
+
+    @Override
+    public AppiumDriver<MobileElement> getDriver() {
+        if (appiumDriver == null)
+            try {
+                appiumDriver = new AppiumDriver<>(new URL(getMobileFarmUrl()), capabilities);
+            } catch (MalformedURLException e) {
+                MyLogger.error(e.getMessage());
+            }
+        appiumDriver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
+        return appiumDriver;
+    }
+
+    @Override
+    public void closeDriver() {
+        appiumDriver.closeApp();
+        appiumDriver.removeApp(getMobileAppPackage());
+        Optional.ofNullable(appiumDriver).ifPresent(RemoteWebDriver::quit);
+    }
+}
