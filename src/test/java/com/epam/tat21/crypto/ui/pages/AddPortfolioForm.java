c0775a31fd4694a40189c3e555b03620bdcb704b
Natallia Karneyeva <Natallia_Karneyeva1@epam.com>
2019-07-15 18:47:24 +0300

    change js

--- src/test/java/com/epam/tat21/crypto/ui/pages/AddPortfolioForm.java
+++ src/test/java/com/epam/tat21/crypto/ui/pages/AddPortfolioForm.java
@@ -1,6 +1,7 @@
 package com.epam.tat21.crypto.ui.pages;
 
 import com.epam.tat21.crypto.ui.service.TestDataReader;
+import com.epam.tat21.crypto.ui.utils.MyLogger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -12,109 +13,112 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AddPortfolioForm extends HeaderPage {
 
-	private final String BASE_URL = TestDataReader.getApplicationUrl() + "portfolio/";
+    private final String BASE_URL = TestDataReader.getApplicationUrl() + "portfolio/";
 
-	@FindBy(xpath = "//input[@ng-model='newPortfolio.Name']")
-	private WebElement inputPortfolioName;
+    @FindBy(xpath = "//input[@ng-model='newPortfolio.Name']")
+    private WebElement inputPortfolioName;
 
-	@FindBy(xpath = "//md-select[@name='currency']//span")
-	private WebElement dropdownCurrency;
+    @FindBy(xpath = "//md-select[@name='currency']//span")
+    private WebElement dropdownCurrency;
 
-	@FindBy(xpath = "//textarea[@ng-model='newPortfolio.Description']")
-	private WebElement textareaDiscription;
+    @FindBy(xpath = "//textarea[@ng-model='newPortfolio.Description']")
+    private WebElement textareaDiscription;
 
-	@FindBy(xpath = "//span[text()=' Private Portfolio ']")
-	private WebElement radiobuttonPrivatePortfolio;
+    @FindBy(xpath = "//span[text()=' Private Portfolio ']")
+    private WebElement radiobuttonPrivatePortfolio;
 
-	@FindBy(xpath = "//span[text()=' Public Portfolio ']")
-	private WebElement radiobuttonPublicPortfolio;
+    @FindBy(xpath = "//span[text()=' Public Portfolio ']")
+    private WebElement radiobuttonPublicPortfolio;
 
-	@FindBy(xpath = "//span[contains(text(), 'Create')]")
-	private WebElement buttonCreate;
+    @FindBy(xpath = "//span[contains(text(), 'Create')]")
+    private WebElement buttonCreate;
 
-	@FindBy(xpath = "//md-icon[@aria-label='Close dialog']")
-	private WebElement buttonCloseFormAddPortfolio;
-	
-	@FindBy(xpath = "//span[contains(text(), 'Update')]")
-	private WebElement buttonUpdatePortfolio;
+    @FindBy(xpath = "//md-icon[@aria-label='Close dialog']")
+    private WebElement buttonCloseFormAddPortfolio;
 
-	@FindBy(xpath = "//span[contains(text(), 'Delete')]")
-	private WebElement buttonDeletePortfolio;
+    @FindBy(xpath = "//span[contains(text(), 'Update')]")
+    private WebElement buttonUpdatePortfolio;
 
-	public AddPortfolioForm(WebDriver driver) {
-		super(driver);
-	}
+    @FindBy(xpath = "//span[contains(text(), 'Delete')]")
+    private WebElement buttonDeletePortfolio;
 
-	@Override
-	public AddPortfolioForm openPage() {
-		driver.navigate().to(BASE_URL);
-		return this;
-	}
+    public AddPortfolioForm(WebDriver driver) {
+        super(driver);
+    }
 
-	public WebElement getElementCurrency(String currency) {
-		String xpathForGetCurrency = "(//md-option[@value='" + currency + "']/div[1]/span)[2]/../..";
-		return driver.findElement(By.xpath(xpathForGetCurrency));
-	}
+    @Override
+    public AddPortfolioForm openPage() {
+        driver.navigate().to(BASE_URL);
+        return this;
+    }
 
-	public PortfolioPage scroll(WebElement element) {
-		Actions action = new Actions(driver);
-		action.moveToElement(element).build().perform();
-		return new PortfolioPage(driver);
-	}
+    public WebElement getElementCurrency(String currency) {
+        String xpathForGetCurrency = "(//md-option[@value='" + currency + "']/div[1]/span)[2]/../..";
+        return driver.findElement(By.xpath(xpathForGetCurrency));
+    }
 
-	private static final String PORTFOLIO_CURRENCY_LOCATOR = "(//md-option[@value='%s']/div[1]/span)[2]";
+    public PortfolioPage scroll(WebElement element) {
+        Actions action = new Actions(driver);
+        action.moveToElement(element).build().perform();
+        return new PortfolioPage(driver);
+    }
 
-	@FindBy(xpath = "(//div[contains(@id,'select_container')])[2]")
-	private WebElement portfolioCurrencyDropdown;
+    private static final String PORTFOLIO_CURRENCY_LOCATOR = "(//md-option[@value='%s']/div[1]/span)[2]";
 
-	private void getPortfolioName() {
-		waitForElementVisible(inputPortfolioName);
-		String actualClass = inputPortfolioName.getAttribute("class");
-		System.out.println("actualClass " + actualClass);
-		String actualID = inputPortfolioName.getAttribute("id");
-		System.out.println("actualID " + actualID);
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		String actualPortfolioName = String.valueOf(js.executeScript("document.getElementById('" + actualID + "')"));
-		System.out.println(actualPortfolioName);
-	}
+    @FindBy(xpath = "(//div[contains(@id,'select_container')])[2]")
+    private WebElement portfolioCurrencyDropdown;
 
-	public PortfolioPage createNewPortfolio(String name, String currency, String description) {
-		waitForElementVisible(inputPortfolioName);
-		Actions action = new Actions(driver);
-		action.sendKeys(inputPortfolioName, name).build().perform();
-		getPortfolioName();
-//		try {
-//			Thread.sleep(5000);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
+    private String getPortfolioName() {
+        waitForElementVisible(inputPortfolioName);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String actualPortfolioName = (String) js.executeScript("return arguments[0].value;", inputPortfolioName);
+        System.out.println("get name from line - " + js.executeScript("return arguments[0].value;", inputPortfolioName));
+        return actualPortfolioName;
+    }
 
-		dropdownCurrency.click();
-		new WebDriverWait(driver, 5).until(ExpectedConditions
-				.attributeToBe(portfolioCurrencyDropdown, "class", "md-select-menu-container md-active md-clickable"));
-		WebElement portfolioCurrency = driver.findElement(By
-				.xpath(String.format(PORTFOLIO_CURRENCY_LOCATOR, currency)));
-		waitForElementClickable(portfolioCurrency);
-		portfolioCurrency.click();
-		textareaDiscription.sendKeys(description);
-		waitForElementClickable(buttonCreate);
-		buttonCreate.click();
-		return new PortfolioPage(driver);
-	}
+    private void assurePortfolioName(String actualName, String expectedName) {
+        if (actualName.equals(expectedName)) {
+            MyLogger.info("Inputed name of portfolio is correct and contains all letters.");
+        }
+        if (!actualName.equals(expectedName)) {
+            Actions action = new Actions(driver);
+            action.sendKeys(inputPortfolioName, expectedName).build().perform();
+        }
+    }
+
+    public PortfolioPage createNewPortfolio(String name, String currency, String description) {
+        waitForElementVisible(inputPortfolioName);
+        inputPortfolioName.click();
+        inputPortfolioName.sendKeys(name);
+//        Actions action = new Actions(driver);
+//        action.sendKeys(inputPortfolioName, name).build().perform();
+        assurePortfolioName(getPortfolioName(), name);
+        dropdownCurrency.click();
+        new WebDriverWait(driver, 5).until(ExpectedConditions
+                .attributeToBe(portfolioCurrencyDropdown, "class", "md-select-menu-container md-active md-clickable"));
+        WebElement portfolioCurrency = driver.findElement(By
+                .xpath(String.format(PORTFOLIO_CURRENCY_LOCATOR, currency)));
+        waitForElementClickable(portfolioCurrency);
+        portfolioCurrency.click();
+        textareaDiscription.sendKeys(description);
+        waitForElementClickable(buttonCreate);
+        buttonCreate.click();
+        return new PortfolioPage(driver);
+    }
 
 
-	public PortfolioPage editUserPortfolio(String name) {
-		waitForElementVisible(inputPortfolioName);
-		inputPortfolioName.clear();
-		inputPortfolioName.sendKeys(name);
-		waitForElementClickable(buttonUpdatePortfolio);
-		buttonUpdatePortfolio.click();
-		return new PortfolioPage(driver);
-	}
-	
-	public PortfolioPage deleteUserPortfolio() {
-		waitForElementClickable(buttonDeletePortfolio);
-		buttonDeletePortfolio.click();
-		return new PortfolioPage(driver);
-	}
+    public PortfolioPage editUserPortfolio(String name) {
+        waitForElementVisible(inputPortfolioName);
+        inputPortfolioName.clear();
+        inputPortfolioName.sendKeys(name);
+        waitForElementClickable(buttonUpdatePortfolio);
+        buttonUpdatePortfolio.click();
+        return new PortfolioPage(driver);
+    }
+
+    public PortfolioPage deleteUserPortfolio() {
+        waitForElementClickable(buttonDeletePortfolio);
+        buttonDeletePortfolio.click();
+        return new PortfolioPage(driver);
+    }
 }
