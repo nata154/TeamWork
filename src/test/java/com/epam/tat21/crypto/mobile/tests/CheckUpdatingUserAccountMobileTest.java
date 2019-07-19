98fab204df620bbf5b2dd347e88f6caa03f4932a
Natallia Karneyeva <Natallia_Karneyeva1@epam.com>
2019-07-19 18:39:38 +0300

    refactored CheckUpdatingUserAccountMobileTest

--- src/test/java/com/epam/tat21/crypto/mobile/tests/CheckUpdatingUserAccountMobileTest.java
+++ src/test/java/com/epam/tat21/crypto/mobile/tests/CheckUpdatingUserAccountMobileTest.java
@@ -3,17 +3,13 @@ package com.epam.tat21.crypto.mobile.tests;
 import com.epam.testng.JIRATestKey;
 import org.testng.annotations.Test;
 
-public class CheckUpdatingUserAccountMobileTest extends PreConditionsOfMobileTest{
-
-    private static final int COUNT_OF_GENERATED_POSTFIX_LETTERS = 3;
+public class CheckUpdatingUserAccountMobileTest extends PreConditionsOfMobileTest {
 
     @JIRATestKey(key = "EPMFARMATS-9610")
     @Test
     public void checkUpdatingUserAccountTest() {
         mobSteps.loginUser();
-        mobSteps.clickUserAccount();
-
-//        Assert.assertEquals("Wrong text in pop-up after saving changes in user general tab",
-//                "Wrong text in pop-up after saving changes in user general tab");
+        //Cryptocompare application doesn't have such function to update user account
+        mobSteps.clickUserAccountAndLogOut();
     }
 }
