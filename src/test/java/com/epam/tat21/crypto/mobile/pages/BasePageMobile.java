0ce0f4851b96cbbd19642d481d61ff3bb50a8cd4
Natallia Karneyeva <Natallia_Karneyeva1@epam.com>
2019-07-19 15:31:30 +0300

    create BasePageMobile

--- src/test/java/com/epam/tat21/crypto/mobile/pages/BasePageMobile.java
+++ src/test/java/com/epam/tat21/crypto/mobile/pages/BasePageMobile.java
@@ -5,23 +5,11 @@ import io.appium.java_client.MobileElement;
 import org.openqa.selenium.support.PageFactory;
 
 public abstract class BasePageMobile {
-    protected AppiumDriver<MobileElement> driver;
-    private static final int WAIT_FOR_ELEMENT_SECONDS = 15;
 
-//    public abstract BasePageMobile openMobilePage();
+    protected AppiumDriver<MobileElement> driver;
 
     public BasePageMobile(AppiumDriver<MobileElement> driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
-
-//    protected void waitForElementVisible(WebElement element) {
-//        new WebDriverWait(driver, WAIT_FOR_ELEMENT_SECONDS)
-//                .until(ExpectedConditions.visibilityOf(element));
-//    }
-//
-//    protected void waitForElementClickable(WebElement element) {
-//        new WebDriverWait(driver, WAIT_FOR_ELEMENT_SECONDS)
-//                .until(ExpectedConditions.elementToBeClickable(element));
-//    }
 }
