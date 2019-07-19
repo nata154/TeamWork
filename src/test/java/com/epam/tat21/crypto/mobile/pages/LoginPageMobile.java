e5d20c470c779f9bb98c8de4078d2b0f1a344532
Natallia Karneyeva <Natallia_Karneyeva1@epam.com>
2019-07-19 17:20:34 +0300

    create some FindBy elements

--- src/test/java/com/epam/tat21/crypto/mobile/pages/LoginPageMobile.java
+++ src/test/java/com/epam/tat21/crypto/mobile/pages/LoginPageMobile.java
@@ -4,11 +4,22 @@ import com.epam.tat21.crypto.ui.businessObjects.User;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 import java.util.List;
 
 
 public class LoginPageMobile extends BasePageMobile{
+
+    @FindBy(xpath = "//android.widget.TextView[@text='Log in']")
+    private WebElement loginFunction;
+
+    @FindBy(xpath = "//android.widget.EditText[@text='Your e-mail']")
+    private WebElement loginFieldName;
+
+    @FindBy(xpath = "//android.widget.EditText[@text='Password']")
+    private WebElement loginFieldPassword;
 
     public LoginPageMobile(AppiumDriver<MobileElement> driver) {
         super(driver);
@@ -20,23 +31,21 @@ public class LoginPageMobile extends BasePageMobile{
         return this;
     }
 
-    public LoginPageMobile clickLoginFunction() {
-        List<MobileElement> loginFunction = driver.findElements(By.className("android.view.ViewGroup"));
-        loginFunction.get(1).click();
-        loginFunction.get(7).click();
+    public LoginPageMobile clickLoginChoice() {
+        loginFunction.click();
         return this;
     }
 
     public LoginPageMobile fillLoginForm(User user) {
-        List<MobileElement> loginFields = driver.findElements(By.className("android.widget.EditText"));
-        loginFields.get(0).click();
-        loginFields.get(0).sendKeys(user.getUserName());
-        loginFields.get(1).click();
-        loginFields.get(1).sendKeys(user.getUserPassword());
+        loginFieldName.click();
+        loginFieldName.sendKeys(user.getUserName());
+        loginFieldPassword.click();
+        loginFieldPassword.sendKeys(user.getUserPassword());
         return this;
     }
 
     public LoginPageMobile clickLoginButton() {
+        //login button doesn't have special text
         List<MobileElement> loginButton = driver.findElements(By.className("android.view.ViewGroup"));
         loginButton.get(6).click();
         return this;
@@ -44,7 +53,7 @@ public class LoginPageMobile extends BasePageMobile{
 
     public MainCryptoComparePageMobile login(User user) {
         skipPreview();
-        clickLoginFunction();
+        clickLoginChoice();
         fillLoginForm(user);
         clickLoginButton();
         return new MainCryptoComparePageMobile(driver);
