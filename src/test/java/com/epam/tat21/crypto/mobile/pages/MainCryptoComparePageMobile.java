dfdeae1f8b83f47f4824d7efc1efcfda1018f57c
Natallia Karneyeva <Natallia_Karneyeva1@epam.com>
2019-07-19 15:31:55 +0300

    create MainCryptoComparePageMobile

--- src/test/java/com/epam/tat21/crypto/mobile/pages/MainCryptoComparePageMobile.java
+++ src/test/java/com/epam/tat21/crypto/mobile/pages/MainCryptoComparePageMobile.java
@@ -2,10 +2,20 @@ package com.epam.tat21.crypto.mobile.pages;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import org.openqa.selenium.By;
+
+import java.util.List;
 
 public class MainCryptoComparePageMobile extends BasePageMobile {
 
     public MainCryptoComparePageMobile(AppiumDriver<MobileElement> driver) {
         super(driver);
     }
+
+    public MainCryptoComparePageMobile clickUserIcon() {
+        List<MobileElement> userImage = driver.findElements(By.className("android.view.ViewGroup"));
+        userImage.get(0).click();
+        return this;
+    }
 }
+
