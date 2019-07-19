06ac8db78c13aebaffb4141e4ac5aa9728b02eb3
Natallia Karneyeva <Natallia_Karneyeva1@epam.com>
2019-07-19 15:32:36 +0300

    create CheckUpdatingUserAccountMobileTest

--- src/test/java/com/epam/tat21/crypto/mobile/tests/CheckUpdatingUserAccountMobileTest.java
+++ src/test/java/com/epam/tat21/crypto/mobile/tests/CheckUpdatingUserAccountMobileTest.java
@@ -1,7 +1,6 @@
 package com.epam.tat21.crypto.mobile.tests;
 
 import com.epam.testng.JIRATestKey;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CheckUpdatingUserAccountMobileTest extends PreConditionsOfMobileTest{
@@ -11,19 +10,10 @@ public class CheckUpdatingUserAccountMobileTest extends PreConditionsOfMobileTes
     @JIRATestKey(key = "EPMFARMATS-9610")
     @Test
     public void checkUpdatingUserAccountTest() {
-//        String firstNameToChange = RandomString.generateRandomUserFirstName(COUNT_OF_GENERATED_POSTFIX_LETTERS);
-//        String surnameToChange = RandomString.generateRandomUserSurname(COUNT_OF_GENERATED_POSTFIX_LETTERS);
-//        String expectedInfoInPopupWhenSuccessfully = "Successfully updated your details";
-//
-//        steps.loginUser();
-//        steps.goToUserAccountProfile();
-//        steps.changeAndSaveFirstNameSurnameInUserAccount(firstNameToChange, surnameToChange);
-//        String infoInPopup = steps.getInfoFromPopupWindowAfterSavingChangesInUserAccount();
-//
-//        Assert.assertEquals(infoInPopup, expectedInfoInPopupWhenSuccessfully,
+        mobSteps.loginUser();
+        mobSteps.clickUserAccount();
+
+//        Assert.assertEquals("Wrong text in pop-up after saving changes in user general tab",
 //                "Wrong text in pop-up after saving changes in user general tab");
-//mobSteps.loginUser();
-        Assert.assertEquals("Wrong text in pop-up after saving changes in user general tab",
-                "Wrong text in pop-up after saving changes in user general tab");
     }
 }
