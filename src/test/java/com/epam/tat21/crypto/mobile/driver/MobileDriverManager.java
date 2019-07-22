6a6be72d0161d60e3236e097065a748a05683026
Natallia Karneyeva <Natallia_Karneyeva1@epam.com>
2019-07-22 12:50:04 +0300

    create MobileDriverManager

---
+++ src/test/java/com/epam/tat21/crypto/mobile/driver/MobileDriverManager.java
@@ -0,0 +1,23 @@
+package com.epam.tat21.crypto.mobile.driver;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+public class MobileDriverManager {
+
+    private static AppiumDriver<MobileElement> driver;
+
+    public static  MobileDriverFactory getMobileDriverFactory() {
+        if (driver == null) {
+            switch (System.getProperty("mobile")) {
+                case "local":
+                    return new MobileLocalDriver();
+                case "farm":
+                    return new MobileDriverForFarm();
+                default:
+                    return new MobileLocalDriver();
+            }
+        }
+        return new MobileLocalDriver();
+    }
+}
