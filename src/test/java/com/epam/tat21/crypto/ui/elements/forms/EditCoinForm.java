6c88a7b186d259a10ce9fae8636faccac346a62b
Dzmitry Panimash <dv.panimash@gmail.com>
2019-07-22 12:30:31 +0300

    created editCoinForm

---
+++ src/test/java/com/epam/tat21/crypto/ui/elements/forms/EditCoinForm.java
@@ -0,0 +1,50 @@
+package com.epam.tat21.crypto.ui.elements.forms;
+
+import com.epam.tat21.crypto.ui.driver.DriverManager;
+import com.epam.tat21.crypto.ui.elements.buttons.BaseButton;
+import com.epam.tat21.crypto.ui.elements.inputs.BaseInput;
+import com.epam.tat21.crypto.ui.utils.WaitConditions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+import ru.yandex.qatools.htmlelements.element.HtmlElement;
+
+@FindBy(xpath = "//div[@class='md-dialog-content']")
+public class EditCoinForm extends HtmlElement {
+
+    private WebDriver driver = DriverManager.getWebDriverFactory().getDriver();
+
+    @FindBy(xpath = "//input[@type='search']")
+    private BaseInput searchCoinField;
+
+    @FindBy(xpath = "//input[@name='amount']")
+    private BaseInput amountField;
+
+    @FindBy(xpath = "//input[@name='buyPrice']")
+    private BaseInput buyPriceField;
+
+    @FindBy(xpath = "//span[contains(text(), 'Update')]")
+    private BaseButton updateCoinButton;
+
+    @FindBy(xpath = "//span[contains(text(), 'Delete')]")
+    private BaseButton deleteCoinButton;
+
+    public void inputAmountOfCoinInField(String amount) {
+        WaitConditions.waitForClickableOfElement(amountField, driver, 5);
+        amountField.inputText(amount);
+    }
+
+    public void inputBuyPriceOfCoinInField(String price) {
+        WaitConditions.waitForClickableOfElement(buyPriceField, driver, 5);
+        buyPriceField.inputText(price);
+    }
+
+    public void clickUpdateCoinButton () {
+        WaitConditions.waitForClickableOfElement(updateCoinButton, driver, 5);
+        updateCoinButton.click();
+    }
+
+    public void clickDeleteCoinButton () {
+        WaitConditions.waitForClickableOfElement(deleteCoinButton, driver, 5);
+        deleteCoinButton.click();
+    }
+}
