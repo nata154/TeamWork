e47a28607df068138caf29857b5c14f6c30ad39f
Natallia Karneyeva <Natallia_Karneyeva1@epam.com>
2019-07-24 13:40:59 +0300

    refactoring MobSteps()

--- src/test/java/com/epam/tat21/crypto/mobile/steps/MobSteps.java
+++ src/test/java/com/epam/tat21/crypto/mobile/steps/MobSteps.java
@@ -5,7 +5,6 @@ import com.epam.tat21.crypto.mobile.pages.LoginPageMobile;
 import com.epam.tat21.crypto.mobile.pages.MainCryptoComparePageMobile;
 import com.epam.tat21.crypto.ui.businessObjects.User;
 import com.epam.tat21.crypto.ui.service.UserCreator;
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -20,24 +19,18 @@ public class MobSteps {
         this.driver = MobileDriverManager.getMobileDriverFactory().getDriver();
     }
 
-    @Given("^Get driver$")
-    public void getDriver() {
-        driver = MobileDriverManager.getMobileDriverFactory().getDriver();
-    }
-
-    @And("^Close driver$")
     public void closeDevice() {
         MobileDriverManager.getMobileDriverFactory().closeDriver();
     }
 
-    @When("^I login user in crypto application$")
+    @Given("^I login user in crypto application$")
     public MainCryptoComparePageMobile loginUser() {
         User user = UserCreator.withCredentialsFromProperty();
         return new LoginPageMobile(driver)
                 .login(user);
     }
 
-    @And("^I click user account and log out$")
+    @When("^I click user account and log out$")
     public MainCryptoComparePageMobile clickUserAccountAndLogOut() {
         return new MainCryptoComparePageMobile(driver)
                 .clickUserAccountIcon()
