f0b75e1615655a3a93b7588478f49d1c66e83ee6
Dzmitry Panimash <Dzmitry_Panimash@epam.com>
2019-08-01 10:48:38 +0300

    user portfolio tests were refactored

--- src/test/java/com/epam/tat21/crypto/ui/tests/UserPortfolioTest.java
+++ src/test/java/com/epam/tat21/crypto/ui/tests/UserPortfolioTest.java
@@ -4,6 +4,7 @@ import com.epam.tat21.crypto.ui.businessObjects.Coin;
 import com.epam.tat21.crypto.ui.service.CommonConditions;
 import com.epam.tat21.crypto.ui.utils.RandomString;
 import com.epam.testng.JIRATestKey;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
@@ -13,28 +14,36 @@ public class UserPortfolioTest extends CommonConditions {
 	private static final int COUNT_OF_SYMBOLS = 5;
 	private Coin coin = Coin.BTC;
 	private String currency = coin.getAbbreviationCoin();
+    private static final String description = RandomString.getRandomString(COUNT_OF_SYMBOLS);
+    private static final String portfolioName = RandomString.getRandomString(COUNT_OF_SYMBOLS);
+
+    @BeforeClass
+    public void logIn() {
+        steps.loginUser();
+    }
 
 	@JIRATestKey(key = "EPMFARMATS-9258")
 	@Test
 	public void portfolioCreationTest() {
-		String description = RandomString.getRandomString(COUNT_OF_SYMBOLS);
-		String portfolioName = RandomString.getRandomString(COUNT_OF_SYMBOLS);
-		steps.loginUser();
 		steps.createUserPortfolio(portfolioName, currency, description);
 		assertTrue(steps.isPortfolioPresent(portfolioName));
+        steps.deleteUserPortfolio();
 	}
 
 	@JIRATestKey(key = "EPMFARMATS-9266")
-	@Test(dependsOnMethods = { "portfolioCreationTest" })
+    @Test
 	public void portfolioEditingTest() {
+        steps.createUserPortfolio(portfolioName, currency, description);
 		String changedName = RandomString.getRandomString(COUNT_OF_SYMBOLS);
 		steps.changeUserPortfolioName(changedName);
 		assertTrue(steps.isPortfolioPresent(changedName));
+        steps.deleteUserPortfolio();
 	}
 
 	@JIRATestKey(key = "EPMFARMATS-9264")
-	@Test(dependsOnMethods = { "portfolioEditingTest" })
+    @Test
 	public void portfolioDeletingTest() {
+        steps.createUserPortfolio(portfolioName, currency, description);
 		steps.deleteUserPortfolio();
 		assertTrue(steps.isPortfolioDelete());
 	}
