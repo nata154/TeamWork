6a2dbd5b062b528be15c8a396ea436aa52cdd667
Dzmitry Panimash <Dzmitry_Panimash@epam.com>
2019-08-01 10:47:59 +0300

    ui steps were refactored

--- src/test/java/com/epam/tat21/crypto/ui/steps/Steps.java
+++ src/test/java/com/epam/tat21/crypto/ui/steps/Steps.java
@@ -1,56 +1,50 @@
 package com.epam.tat21.crypto.ui.steps;
 
-import static com.epam.tat21.crypto.ui.service.GlobalConstants.REGEX_FOR_SPACES;
-import static org.testng.Assert.assertTrue;
-
-import java.util.List;
-import java.util.stream.IntStream;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.testng.Assert;
-
 import com.epam.tat21.crypto.ui.businessObjects.Coin;
 import com.epam.tat21.crypto.ui.businessObjects.Countries;
 import com.epam.tat21.crypto.ui.businessObjects.User;
 import com.epam.tat21.crypto.ui.driver.DriverManager;
-import com.epam.tat21.crypto.ui.pages.CoinsPage;
-import com.epam.tat21.crypto.ui.pages.ExchangesPage;
-import com.epam.tat21.crypto.ui.pages.MainCryptoComparePage;
-import com.epam.tat21.crypto.ui.pages.NewsPage;
-import com.epam.tat21.crypto.ui.pages.PortfolioPage;
-import com.epam.tat21.crypto.ui.pages.UserAccountPage;
+import com.epam.tat21.crypto.ui.pages.*;
 import com.epam.tat21.crypto.ui.service.UserCreator;
 import com.epam.tat21.crypto.ui.utils.MyLogger;
 import com.epam.tat21.crypto.ui.utils.RandomString;
-
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static com.epam.tat21.crypto.ui.service.GlobalConstants.REGEX_FOR_SPACES;
+import static org.testng.Assert.assertTrue;
 
 public class Steps {
-	
+
     private static final int COUNT_OF_GENERATED_POSTFIX_LETTERS = 3;
     private static final int COUNT_OF_SYMBOLS = 5;
     private static final int LIMIT = 1000;
     private static final int COUNT_OF_NEWS = 50;
-	private Coin coin = Coin.BTC;
-	private String currency = coin.getAbbreviationCoin();
-	private String portfolioName = RandomString.getRandomString(COUNT_OF_SYMBOLS);
-	private String changedName = RandomString.getRandomString(COUNT_OF_SYMBOLS);
-	
+    private Coin coin = Coin.BTC;
+    private String currency = coin.getAbbreviationCoin();
+    private String portfolioName = RandomString.getRandomString(COUNT_OF_SYMBOLS);
+    private String changedName = RandomString.getRandomString(COUNT_OF_SYMBOLS);
+
     private WebDriver driver;
     private ExchangesPage exchangesPage;
     private NewsPage newsPage;
     private PortfolioPage portfolioPage;
 
     public Steps() {
-        this.driver = DriverManager.getWebDriverFactory().getDriver();
+        System.out.println("STEPS CREATED + hashcode: " + this.hashCode() + " THREAD: " + Thread.currentThread().getName() + " DRIVER: ");
+        this.driver = DriverManager.getDriver();
     }
 
     public void closeBrowser() {
-        DriverManager.getWebDriverFactory().closeDriver();
+        DriverManager.closeDriver();
     }
 
     @Given("I login user")
@@ -72,16 +66,16 @@ public class Steps {
                 .enterNewFirstNameAndSurname(firstNameToChange, surnameToChange)
                 .clickButtonSaveChanges();
     }
-    
+
     @And("Change And save First name and Surname in User Account")
     public UserAccountPage changeAndSaveDataInUserAccount() {
-    	String firstNameToChange = RandomString.generateRandomUserFirstName(COUNT_OF_GENERATED_POSTFIX_LETTERS);
+        String firstNameToChange = RandomString.generateRandomUserFirstName(COUNT_OF_GENERATED_POSTFIX_LETTERS);
         String surnameToChange = RandomString.generateRandomUserSurname(COUNT_OF_GENERATED_POSTFIX_LETTERS);
         return new UserAccountPage(driver)
                 .enterNewFirstNameAndSurname(firstNameToChange, surnameToChange)
                 .clickButtonSaveChanges();
     }
-    
+
     @And("Get info from in User Account")
     public String getInfoFromPopupWindowAfterSavingChangesInUserAccount() {
         return new UserAccountPage(driver).getInfoFromPopupWindow();
@@ -92,7 +86,7 @@ public class Steps {
         return exchangesPage = new ExchangesPage(driver).
                 openPage();
     }
-    
+
     public ExchangesPage filterByCountry(Countries country) {
         return exchangesPage.
                 clickOnCountryDropdown().
@@ -105,7 +99,7 @@ public class Steps {
                 clickOnCountryDropdown().
                 selectCountryInDropdown(country);
     }
-    
+
     public int getFromFilteredPageNumberOfResultsWith(Countries country) {
         return exchangesPage.getFromFilteredPageAllResultsWith(country).size();
     }
@@ -124,17 +118,17 @@ public class Steps {
     public int getNumberOfExchangesFromCountryBadge() {
         return exchangesPage.getNumberOfExchangesInBadge();
     }
-    
+
     @Then("Is number of results equals number from ([^\"]*) label")
     public void isExchangesNumberOnFilteredPageEqualNumberFromLabel(String country) {
-    	int numberOfResultWithCountry = getFromFilteredPageNumberOfResultsWith(country);
-    	int numberOfAllCountryLabels = getAllCountryLabelsFromFilteredPage();
-    	Assert.assertEquals(numberOfResultWithCountry, numberOfAllCountryLabels);
+        int numberOfResultWithCountry = getFromFilteredPageNumberOfResultsWith(country);
+        int numberOfAllCountryLabels = getAllCountryLabelsFromFilteredPage();
+        Assert.assertEquals(numberOfResultWithCountry, numberOfAllCountryLabels);
     }
-    
+
     @Then("Is number of results equals number from ([^\"]*) badge")
     public void isExchangesNumberOnFilteredPageEqualNumberFromBadge(String country) {
-    	int numberOfResultWithCountry = getFromFilteredPageNumberOfResultsWith(country);
+        int numberOfResultWithCountry = getFromFilteredPageNumberOfResultsWith(country);
         int numberFromCountryBadge = getNumberOfExchangesFromCountryBadge();
         Assert.assertEquals(numberOfResultWithCountry, numberFromCountryBadge);
     }
@@ -144,7 +138,7 @@ public class Steps {
         return newsPage = new NewsPage(driver).
                 openPage();
     }
-    
+
     public NewsPage filterByCoin(Coin coin) {
         return newsPage.goToCoinNews(coin);
     }
@@ -153,7 +147,7 @@ public class Steps {
     public NewsPage filterByCoin(String abbreviationCoin) {
         return newsPage.goToCoinNews(abbreviationCoin);
     }
-    
+
     public int getAllCoinNewsFromFilteredPage(Coin coin) {
         return newsPage.getNumberOfNewsForCoin(coin);
     }
@@ -162,13 +156,13 @@ public class Steps {
     public int getAllCoinNewsFromFilteredPage(String abbreviationCoin) {
         return newsPage.getNumberOfNewsForCoin(abbreviationCoin);
     }
-    
+
     @Then("Are the news sorted by ([^\"]*)")
     public void areTheNewsSortedByCoin(String abbreviationCoin) {
-    	int numberOfResultWithCoin = getAllCoinNewsFromFilteredPage(abbreviationCoin);
-    	 Assert.assertEquals(numberOfResultWithCoin, COUNT_OF_NEWS);
+        int numberOfResultWithCoin = getAllCoinNewsFromFilteredPage(abbreviationCoin);
+        Assert.assertEquals(numberOfResultWithCoin, COUNT_OF_NEWS);
     }
-    
+
     /**
      * The method below, if the page contains more than 50 titles,
      * returns a subarray of only 50 latest titles, because the api response
@@ -189,42 +183,41 @@ public class Steps {
     }
 
     public PortfolioPage createUserPortfolio(String name, String currency, String description) {
-        return portfolioPage = new MainCryptoComparePage(driver).
-                getHeaderMenu().
-                goToMyPortfolioFromPortfolioTab().
+        portfolioPage = new PortfolioPage(driver);
+        return portfolioPage.openPage().
                 getAddPortfolioForm().
                 createNewPortfolio(name, currency, description);
     }
-    
+
     public boolean isPortfolioPresent(String name) {
         return new PortfolioPage(driver).
                 getPortfolioItemByName(name).
                 isEnabled();
     }
-    
+
     @Then("Is Portfolio present")
     public void isPortfolioPresent() {
-       boolean portfolioPresence = new PortfolioPage(driver).
-    		   getPortfolioItemByName(portfolioName).
+        boolean portfolioPresence = new PortfolioPage(driver).
+                getPortfolioItemByName(portfolioName).
                 isEnabled();
-       assertTrue(portfolioPresence);
+        assertTrue(portfolioPresence);
     }
-    
+
     @When("Create User Portfolio")
     public PortfolioPage createUserPortfolio() {
-    	String description = RandomString.getRandomString(COUNT_OF_SYMBOLS);
-		
-    	 return portfolioPage = new MainCryptoComparePage(driver).
-                 getHeaderMenu().
-                 goToMyPortfolioFromPortfolioTab().
-                 getAddPortfolioForm().
-                 createNewPortfolio(portfolioName, currency, description);
+        String description = RandomString.getRandomString(COUNT_OF_SYMBOLS);
+
+        return portfolioPage = new MainCryptoComparePage(driver).
+                getHeaderMenu().
+                goToMyPortfolioFromPortfolioTab().
+                getAddPortfolioForm().
+                createNewPortfolio(portfolioName, currency, description);
     }
-    
+
     @Then("Is Portfolio with changed name present")
     public void isPortfolioWithChangedNamePresent() {
         boolean presenceChangedPortfolio = new PortfolioPage(driver).
-        		getPortfolioItemByName(changedName).
+                getPortfolioItemByName(changedName).
                 isEnabled();
         assertTrue(presenceChangedPortfolio);
     }
@@ -234,7 +227,7 @@ public class Steps {
                 getEditPortfolioForm().
                 editUserPortfolio(name);
     }
-    
+
     @When("Change User Portfolio name")
     public PortfolioPage changeUserPortfolioName() {
         return portfolioPage.
@@ -243,13 +236,13 @@ public class Steps {
     }
 
     @When("Delete User Portfolio")
-    public PortfolioPage deleteUserPortfolio() {
+    public synchronized PortfolioPage deleteUserPortfolio() {
         return portfolioPage.
                 getEditPortfolioForm().
                 deleteUserPortfolio().
                 confirmDeletion();
     }
-    
+
     public boolean isPortfolioDelete() {
         return portfolioPage.
                 isPortfolioDelete();
@@ -270,28 +263,26 @@ public class Steps {
 
     @Then("I check updating User Account")
     public void checkUpdatingUserAccount() {
-    	String infoInPopup = getInfoFromPopupWindowAfterSavingChangesInUserAccount();
-    	String expectedInfoInPopupWhenSuccessfully = "Successfully updated your details";
-    	 Assert.assertEquals(infoInPopup, expectedInfoInPopupWhenSuccessfully,
+        String infoInPopup = getInfoFromPopupWindowAfterSavingChangesInUserAccount();
+        String expectedInfoInPopupWhenSuccessfully = "Successfully updated your details";
+        Assert.assertEquals(infoInPopup, expectedInfoInPopupWhenSuccessfully,
                 "Wrong text in pop-up after saving changes in user general tab");
     }
 
-    public PortfolioPage addCoinToUserPortfolio(Coin coin, String amount, String price) {
-        return portfolioPage.
-                getAddCoinForm().
-                addCoinInPortfolio(coin, amount, price);
+    public void addCoinToUserPortfolio(Coin coin, String amount, String price) {
+        portfolioPage.getAddCoinForm().addCoinInPortfolio(coin, amount, price);
     }
-    
-    @And ("Add Coin to user portfolio")
+
+    @And("Add Coin to user portfolio")
     public PortfolioPage addCoinToUserPortfolio() {
-    	Coin coin = Coin.BTC;
-    	String amount = RandomString.getRandomNumber(LIMIT); 
-    	String price = RandomString.getRandomNumber(LIMIT);
+        Coin coin = Coin.BTC;
+        String amount = RandomString.getRandomNumber(LIMIT);
+        String price = RandomString.getRandomNumber(LIMIT);
         return portfolioPage.
                 getAddCoinForm().
                 addCoinInPortfolio(coin, amount, price);
     }
-    
+
     public boolean isCoinAdded() {
         return portfolioPage.
                 isCoinAdded();
@@ -309,15 +300,15 @@ public class Steps {
                 getEditCoinForm().
                 editAmountOfCoin(amount);
     }
-    
+
     @And("Change amount of coins")
     public PortfolioPage changeAmountOfCoins() {
-    	String changedAmount = RandomString.getRandomNumber(LIMIT);
+        String changedAmount = RandomString.getRandomNumber(LIMIT);
         return portfolioPage.
                 getEditCoinForm().
                 editAmountOfCoin(changedAmount);
     }
-    
+
     public boolean isAmountOfCoinChanged() {
         return portfolioPage.
                 isAmountOfCoinChanged();
@@ -337,7 +328,7 @@ public class Steps {
                 deleteCoinFromPortfolio().
                 confirmCoinDeletion();
     }
-    
+
     public boolean isCoinDelete() {
         return portfolioPage.
                 isCoinDelete();
